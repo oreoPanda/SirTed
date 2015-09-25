0ef34e124b7a1e1e6e656e5900265468d178b240
Kjell Meyn <Kjell@linux-tvqd.site>
2015-09-26 00:36:57 +0200

    Drop control gut sooooooo :)?

--- main.c
+++ main.c
@@ -10,35 +10,58 @@
 #include "gtk_stuff.h"
 #include "sirted.h"
 
-#define TRUE 1
-#define FALSE 0 
+void init_game_area()
+{
+	int x, y;
+	for( x = 0; x < GAMEWIDTH; x++ )
+	{
+		for( y = 0; y < GAMEHEIGHT; y++ )
+		{
+			game_area[x][y] = 0;
+		}
+	}
+
+	return;
+}
 
 gboolean drop_control(gpointer data)
 {
-	static gboolean first_call = TRUE; 
+	static gboolean first_call = TRUE;
+	int i = 0;  
 	time_t t; 
 	time (&t); 
-	srand ((unsigned int) t); 
-	if(first_call == TRUE)
-	{
-		falling.type = rand()%BLOCK_max; 
-		falling.pos[0] = {5};
-		falling.pos[1] = {0}; 
-		falling.rotation = 0;
-		first_call = FALSE;  	
+	srand ((unsigned int) t);
+	if(falling.pos[1] != 0) 
+	{ 
+		if(first_call == TRUE)
+		{
+			falling.type = rand()%BLOCK_max; 
+			falling.pos[0] = {5};
+			falling.pos[1] = {0}; 
+			falling.rotation = 0;
+			first_call = FALSE;  	
+		}
+		else
+		{
+			falling.type = block_next; 
+			falling.pos[0] = {5};
+			falling.pos[1] = {0}; 
+			falling.rotation = 0; 	
+		}	
+		block_next = rand()%BLOCK_number; 
 	}
-	else
+	
+	if (draw_falling_block () == FALSE)
 	{
-		falling.type = block_next; 
-		falling.pos[0] = {5};
-		falling.pos[1] = {0}; 
-		falling.rotation = 0; 	
-	}	
-
+		return G_SOURCE_CONTINUE;
+	} 
 	
-	draw_falling_block (); 
+	if (falling.pos[i-1] == 0)
+	{
+		falling.pos[1]--; 
+		redraw(); 
+	}
 	
-
 	return G_SOURCE_CONTINUE;
 }
 
@@ -46,11 +69,17 @@ int main (int argc, char *argv[])
 {
 	gtk_init (&argc, &argv);
 
+	init_game_area(); 
+
 	create_window();
 	create_grid();
 	create_button();
+	create_drawing_area();
 	populate_window();
-
+	
+	g_signal_connect (drawing_area, "draw", G_CALLBACK(ready_to_draw), NULL );
+	g_signal_connect ( drawing_area, "configure-event", G_CALLBACK(configure_drawing_area), NULL );
+		
 	g_timeout_add( 750, drop_control, NULL );
 
 	gtk_main();
