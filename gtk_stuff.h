447d3e12de2dd5b033d2347375293819e2c6c688
Jonas <legojonas@gmx.de>
2015-09-26 01:14:24 +0200

    Added BASE

--- gtk_stuff.h
+++ gtk_stuff.h
@@ -1,8 +1,13 @@
 #include <gtk/gtk.h>
 
+#define BASE 20
+
 extern void create_window(void);
 extern void create_grid(void);
 extern void create_button(void);
 extern void create_frame(void);
 
-extern void draw_falling_block(void);
+extern gboolean configure_drawing_area( GtkWidget *widget, GdkEventConfigure *event, gpointer data )
+extern gboolean ready_to_draw( GtkWidget *widget, cairo_t *cr, gpointer data );
+extern gboolean draw_falling_block(void);
+extern gboolean redraw_falling_block(void);
