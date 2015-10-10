00d1400a0a0eeccbf57cb69e7eed3c8d1a158a40
Jonas <legojonas@gmx.de>
2015-10-10 16:18:40 +0200

    Added a semicolon...

--- gtk_stuff.h
+++ gtk_stuff.h
@@ -1,3 +1,8 @@
+/*gtk_stuff.h*/
+
+#ifndef GTK_STUFF_H
+#define GTK_STUFF_H
+
 #include <gtk/gtk.h>
 
 #define BASE 20
@@ -7,7 +12,9 @@ extern void create_grid(void);
 extern void create_button(void);
 extern void create_frame(void);
 
-extern gboolean configure_drawing_area( GtkWidget *widget, GdkEventConfigure *event, gpointer data )
+extern gboolean configure_drawing_area( GtkWidget *widget, GdkEventConfigure *event, gpointer data );
 extern gboolean ready_to_draw( GtkWidget *widget, cairo_t *cr, gpointer data );
 extern gboolean draw_falling_block(void);
 extern gboolean redraw_falling_block(void);
+
+#endif	//GTK_STUFF_H
